feaa922d72a078913a1cf4ca3a29d3de79038dfd
Kyle <n.kyle.ma@gmail.com>
2017-03-04 12:07:24 -0800

    Update

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 
-setup(name='tracker', version=0.01,
+setup(name='tracker2', version=0.02,
       description='Keep track of hours played on Steam',
       url='https://github.com/Kyle-Magee/Tracker', author='Kyle Magee',
       author_email='n.kyle.ma@gmail.com', license='MIT',
-      packages=['tracker'], zip_safe=False)
+      packages=['tracker2'], zip_safe=False)
---
+++ tracker2/config.py
@@ -0,0 +1,10 @@
+steam_key = '7BB96048CC0F37E729B49814F9184B29'
+steam_id = '76561198035883189'
+steam_url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}&format=json'.format(
+    steam_key, steam_id)
+mail_url = 'https://api.mailgun.net/v3/sandbox5ec3142264ec408ca87618cb784f58d9.mailgun.org/messages'
+mail_key = ("api", "key-057b7f5854098c8fb536d36000b58acc")
+user_email = 'n.kyle.ma@gmail.com'
+
+
+secret_key = 'WUBWUBWUB'
